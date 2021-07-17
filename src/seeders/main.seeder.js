7bf78e6a2e32e623ceb216e452de47c54841a8ed
cartory <xXcartoryXx@gmail.com>
2021-07-16 23:33:39 -0400

    seeders

---
+++ src/seeders/main.seeder.js
@@ -0,0 +1,8 @@
+// import ParadasSeeder from "./paradas.seeder"
+
+// const MainSeeder = async (req, res) => {
+//     await ParadasSeeder();
+//     return res.status(200).json({ ok: true })
+// }
+
+// export default MainSeeder
---
+++ src/seeders/paradas.seeder.js
@@ -0,0 +1,12 @@
+// import paradas from './paradas.json';
+// import Parada from '../models/parada';
+
+
+// const ParadasSeeder = async () => {
+//     try {
+//         await Parada.bulkCreate(paradas.busesParadas);
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
+// export default ParadasSeeder
